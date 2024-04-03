e4e17f950d684bfb59cadccc5921d03679cb75fa
Markin2011 <markinw2011@gmail.com>
2024-04-03 12:07:01 +0700

    main

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -163,7 +163,7 @@
         </a>
         <!-- Dropdown - User Information -->
         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-          <a class="dropdown-item" href="/profile">
+          {{-- <a class="dropdown-item" href="/profile">
             <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
             Profile
           </a>
@@ -174,7 +174,7 @@
           <a class="dropdown-item" href="#">
             <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
             Activity Log
-          </a>
+          </a> --}}
           <div class="dropdown-divider"></div>
           {{-- <a class="dropdown-item" href=""> --}}
             <a class="dropdown-item" href="{{ route('logout') }}">
